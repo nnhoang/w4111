9ab58a9dd7005867c17178517d0a6f477f013835
Nam Hoang <nnh2110@columbia.edu>
2015-12-02 19:09:46 -0500

    update query

--- proj2/json.sql
+++ proj2/json.sql
@@ -5,4 +5,9 @@ ON T.list_id = L.lid
 JOIN accessible_user A
 ON L.lid = A.list_id
 WHERE A.account_id = 1
-AND T.optional->>'due'::timestamp < '01/02/2016';
+AND (T.optional->>'due')::timestamp < '01/02/2016';
+
+SELECT *
+FROM task T JOIN accessible_user A
+ON (T.optional->>'assigned_to')::int = A.account_id;
+
