fe50e4f6d3ae3d8520d913652a6ef9ccded97cbf
Nam Hoang <nnh2110@columbia.edu>
2015-12-10 22:11:48 -0500

    proj2 done!

--- proj2.sql
+++ proj2.sql
@@ -1,3 +1,5 @@
+-- Queries
+-- find all tasks that due before the date below and belong to user with id 1
 SELECT *
 FROM task T
 JOIN list L
@@ -6,13 +8,6 @@ JOIN accessible_user A
 ON L.lid = A.list_id
 WHERE A.account_id = 1
 AND (T.optional->>'due')::timestamp < '01/02/2016';
-
-/*
-SELECT *
-FROM task T JOIN accessible_user A
-ON (T.optional->>'assigned_to')::int = A.account_id;
-*/
-
 
 -- find all the task with red label assinged to user 3
 SELECT * 
@@ -53,6 +48,7 @@ CREATE TABLE comment (
   sender int NOT NULL,
   FOREIGN KEY (sender, list_id) REFERENCES accessible_user(account_id, list_id)
 );
+CREATE TABLE task ( tid serial PRIMARY KEY, optional json, name varchar(100) NOT NULL, assigned_to int, last_editor int NOT NULL, list_id int NOT NULL REFERENCES list(lid) ON DELETE CASCADE, status boolean DEFAULT FALSE, when_completed timestamp, CHECK ((status = TRUE AND when_completed is NOT NULL) OR (status = FALSE AND when_completed is NULL)), FOREIGN KEY (assigned_to, list_id) REFERENCES accessible_user(account_id, list_id), FOREIGN KEY (last_editor, list_id) REFERENCES accessible_user(account_id, list_id) ); -- inside the JSON optional (due timestamp, description text)
 
 CREATE TABLE task (
   tid serial PRIMARY KEY,
@@ -101,13 +97,24 @@ CREATE TABLE label_task (
 DROP FUNCTION checkColor() CASCADE;
 CREATE FUNCTION checkColor() RETURNS trigger
 AS $$
+DECLARE
+  color text = NEW.optional->>'color';
+  name text = NEW.optional->>'name';
 BEGIN
-  IF NEW.optional->>'color' in ('blue', 'red', 'green', 'orange', 'white', 'black', 'yellow', 'purple') THEN
-    -- DELETE FROM label WHERE label.lid = NEW.lid;
-    RAISE 'Invalid label color';
-    RETURN NULL;
+  IF char_length(name) < 20 THEN
+    IF color is NULL THEN
+      NEW.optional := concat('{"name": "'::text, name, '", "color": "white"}'::text)::json;
+      RETURN NEW;
+    ELSIF NEW.optional->>'color' in ('blue', 'red', 'green', 'orange', 'white', 'black', 'yellow', 'purple') THEN
+      -- DELETE FROM label WHERE label.lid = NEW.lid;
+      RETURN NEW;
+    ELSE
+      RAISE 'Invalid label color';
+    END IF;
+  ELSE
+    RAISE 'Invalid name';
   END IF;
-  RETURN NEW;
+  RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -119,5 +126,9 @@ FOR EACH ROW
 -- test queries
 -- this one should work
 INSERT INTO label(optional, list_id) VALUES ('{"name": "part3", "color": "blue"}', 20);
--- this one should NOT work
+-- this one should return 'Invalid label color' error
 INSERT INTO label(optional, list_id) VALUES ('{"name": "part3", "color": "abc"}', 20);
+-- this one should return 'Name too long' error
+INSERT INTO label(optional, list_id) VALUES ('{"name": "ijd ajfioja fjiasd fiojasifjoasfasjfioafjasifjiafioasjfiasofjasjfias", "color": "abc"}', 20);
+-- this one should make the color the default color (white) in to optional column
+INSERT INTO label(optional, list_id) VALUES ('{"name": "part3"}', 20);
